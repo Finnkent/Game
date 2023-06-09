8c0d9aa47e006cfbf04c7174e8741b19a6b09314
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-06-09 18:57:49 +0700

    Worldclone.py

--- Worldclone.py
+++ Worldclone.py
@@ -24,19 +24,19 @@ with TelegramClient(sesi_file, api_id, api_hash) as client:
             return
         
         if "Kamu mencari sampah menggunakan Jala" in event.raw_text:
-            Doo = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
+            Doo = await client.get_messages('KampungMaifamXBot', ids=event.message.id)
             time.sleep(33.1)
             await Doo.click(text='KumpulkanSampah')        
             return
           
         if "Kamu mencari sampah menggunakan Pancing" in event.raw_text:
-            Doo = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
+            Doo = await client.get_messages('KampungMaifamXBot', ids=event.message.id)
             time.sleep(16.1)
             await Doo.click(text='KumpulkanSampah')        
             return
           
         if "Kamu mencari sampah menggunakan Tongkat" in event.raw_text:
-            Doo = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
+            Doo = await client.get_messages('KampungMaifamXBot', ids=event.message.id)
             time.sleep(5.1)
             await Doo.click(text='KumpulkanSampah')        
             return
