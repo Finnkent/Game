d27d689020dec54b63e606283f6dc73ad0a5e16a
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-06-09 18:15:24 +0700

    World.py

--- World.py
+++ World.py
@@ -23,9 +23,21 @@ with TelegramClient(sesi_file, api_id, api_hash) as client:
             await Doo.click(text='KumpulkanSampah')        
             return
         
-        if "Kamu mencari sampah" in event.raw_text:
+        if "Kamu mencari sampah menggunakan Jala" in event.raw_text:
             Doo = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
-            time.sleep(31)
+            time.sleep(33.1)
+            await Doo.click(text='KumpulkanSampah')        
+            return
+          
+        if "Kamu mencari sampah menggunakan Pancing" in event.raw_text:
+            Doo = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
+            time.sleep(16.1)
+            await Doo.click(text='KumpulkanSampah')        
+            return
+          
+        if "Kamu mencari sampah menggunakan Tongkat" in event.raw_text:
+            Doo = await client.get_messages('KampungMaifamX4Bot', ids=event.message.id)
+            time.sleep(5.1)
             await Doo.click(text='KumpulkanSampah')        
             return
             
