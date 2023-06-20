692b87553eba8105577fd5f6dabae6380b780a8d
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-06-20 21:17:56 +0700

    modcu.py

--- modcu.py
+++ modcu.py
@@ -12,6 +12,7 @@ api_id = 18850178
 api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
 sesi_file = input("Mau akun mana = ")
 
+mese = input("Kirim pesan apa = ")
 grup = 'mapiamodcuu'
 bot_id = 'TrueMafiaBot'
 lanjut = '/next@TrueMafiaBot'
@@ -19,7 +20,6 @@ quit = '/quit'
 game = '/game@TrueMafiaBot'
 start = '/start@TrueMafiaBot'
 lanjut = '/next@TrueMafiaBot'
-mese = input("kirim pesan apa =")
 
 kon = 0
 
