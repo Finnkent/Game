80d9d72ed7d8a8ac2b463188fbc2b25b36b9c9fd
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-07-01 21:32:13 +0700

    ternak.py

--- ternak.py
+++ ternak.py
@@ -9,7 +9,7 @@ api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
 sesi_file = input('Mau akun mana = ')
 
 dest =('KampungMaifamXBot')
-Ternak = '/pelihara_BayiBabi_152'
+Ternak = '/pelihara_BayiBabi_155'
 #restore = '/restore'
 restore = '/restore_max_confirm'
  
