40a350ce6106d425700eda5012fd69624adae92b
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-07-01 19:36:33 +0700

    ternak.py

--- ternak.py
+++ ternak.py
@@ -6,7 +6,7 @@ from telethon import TelegramClient, events, utils, Button
 
 api_id = 18850178 
 api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
-sesi_file = 'Finnkent'
+sesi_file = input('Mau akun mana = ')
 
 dest =('KampungMaifamXBot')
 Ternak = '/pelihara_BayiBabi_152'
