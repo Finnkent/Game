cc11fddcb76782d84ee3ee1fffe2bca9038bbd7e
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-07-07 07:14:53 +0700

    grup.py

--- grup.py
+++ grup.py
@@ -1,24 +1,37 @@
-from telethon.sync import TelegramClient, events
+import asyncio
+from telethon import TelegramClient, events
 
+# Inisialisasi Client
 api_id = '18850178'
 api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
+client = TelegramClient('Finnkent', api_id, api_hash)
 
-session_name = 'Finnkent'
+group_id = '-1001943370596'
+join_command = '/joinvcg'
 
-group_id = -1001943370596
-join_command = '.joinvcg'
-
-client = TelegramClient(session_name, api_id, api_hash)
-
-
-def start_voice_chat(group):
-    client.send_message(group, join_command)
-
+async def start_voice_chat():
+    await client.send_message(group_id, join_command)
 
 @client.on(events.NewMessage(chats=group_id))
 async def handle_message(event):
     if event.raw_text == join_command and event.sender_id == (await client.get_me()).id:
-await client.start_call(group_id)
+        await client.start_call(group_id)
 
-with client:
-    client.run_until_disconnected()
+@client.on(events.NewMessage(pattern='/mulai', chats=group_id))
+async def handle_start_command(event):
+    await event.reply('Mulai...')
+    await start_voice_chat()
+
+@client.on(events.NewMessage(pattern='/keluarvcg', chats=group_id))
+async def handle_exit_command(event):
+    # Menggunakan metode leave_call untuk keluar dari voice chat group 
+    await client.leave_call(group_id)
+    await event.reply('Keluar dari Voice Chat Group.')
+
+async def main():
+    # Menunggu sampai client siap
+    await client.start()
+    await client.run_until_disconnected()
+
+if name == 'main':
+    asyncio.run(main())
