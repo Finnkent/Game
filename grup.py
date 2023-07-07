c631b3d701467aa55498e875a225330fb586a1a3
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-07-07 07:50:12 +0700

    grup.py

--- grup.py
+++ grup.py
@@ -1,35 +1,37 @@
 import asyncio
 from telethon import TelegramClient, events
 
-# Inisialisasi Client
+# Inisialisasi bot Telegram Client
 api_id = '18850178'
 api_hash = '34d2d64d0bb5827789bc7bf7c0d34b69'
 client = TelegramClient('Finnkent', api_id, api_hash)
 
-group_id = -1001943370596
-join_command = '/joinvcg'
+# Command yang digunakan untuk bergabung ke voice chat
+join_command = '/mulaivcg'
 
-async def start_voice_chat():
+
+async def start_voice_chat(group_id):
     await client.send_message(group_id, join_command)
 
 # Event handler untuk pesan baru dalam grup
-@client.on(events.NewMessage(chats=group_id))
+@client.on(events.NewMessage())
 async def handle_message(event):
-    if event.raw_text == join_command and event.sender_id == (await client.get_me()).id:
-        await client.start_call(group_id)
+    if event.raw_text == join_command and event.is_group and event.sender_id == event.sender_id:
+        await client.start_call(event.chat_id)
 
 # Event handler untuk perintah '/mulai'
-@client.on(events.NewMessage(pattern='/mulai', chats=group_id))
+@client.on(events.NewMessage(pattern='/mulai'))
 async def handle_start_command(event):
-    await event.reply('Mulai...')
-    await start_voice_chat()
+    await event.respond('Mulai...')
+    await start_voice_chat(event.chat_id)
 
 # Event handler untuk perintah '/keluarvcg'
-@client.on(events.NewMessage(pattern='/keluarvcg', chats=group_id))
+@client.on(events.NewMessage(pattern='/keluarvcg'))
 async def handle_exit_command(event):
     # Menggunakan metode leave_call untuk keluar dari voice chat group 
-    await client.leave_call(group_id)
-    await event.reply('Keluar dari Voice Chat Group.')
+    await client.leave_call(event.chat_id)
+    await event.respond('Keluar dari Voice Chat Group.')
 
-client.start()
-client.run_until_disconnected()
+# Menjalankan bot Telegram
+with client:
+    client.run_until_disconnected()
