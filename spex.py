783ab5fade2aa94927c292136d62c4cc9913f818
Finnkent <134615961+Finnkent@users.noreply.github.com>
2023-07-13 21:23:12 +0700

    spex.py

--- spex.py
+++ spex.py
@@ -8,7 +8,7 @@ sesi_file = input('Akun = ')
 
 space = 'SpaceExploration'
 restore = '/restore'
-bot = 'KampungMaifamBot'
+bot = 'KampungMaifamX4Bot'
 
 with TelegramClient(sesi_file, api_id, api_hash) as client:
         client.loop.run_until_complete(client.send_message(bot, space))
